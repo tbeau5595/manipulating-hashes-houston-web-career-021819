5e0d01370ecd317fba05e4b2f75926e25a2b1795
Trey <tbeau5595@hotmail.com>
2019-02-14 23:20:09 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,8 +16,8 @@ def first_challenge
   contacts.each do |person, info|
     info.each do |type, value|
       if type = :favorite_icecream_flavors
-        value.delete_if 
-          
+        value.delete_if do |flavor|
+          flavor = "strawberry"
         end
       end
     end
